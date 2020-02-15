967584ec7021e6bd87848489fd3d007794dd5ea6
Jose_Martinez <martinez022jose@gmail.com>
2020-02-15 16:18:57 -0300

    Agrego archivos html css y js/jquery

---
+++ inicioVentaProductosElectronicos.js
@@ -0,0 +1,67 @@
+$(()=>{
+	document.querySelector('.cajaCategorias .item[data-etiqueta="todos"]').classList.add('categoriaActivada');
+	
+	var categorias = document.querySelectorAll('.item');
+	
+	var imagenes = document.querySelectorAll('.img');
+	
+    //Recorremos las categorias
+
+	categorias.forEach((categoria)=>{
+		
+		//Agregamos un evento al momento de realizar un "click" en cualquier categoria
+		categoria.addEventListener('click',(elementoActual)=>{
+		    
+		    var categoriaActual =elementoActual.target.innerHTML.toLowerCase();
+
+			//Sacamos la clase categoriaActivada al que la tiene actualmente
+			document.querySelector('.cajaCategorias .categoriaActivada').classList.remove('categoriaActivada');
+			//Agregamos la clase categoriaActivada al que realizaron "click"
+			categoria.classList.add('categoriaActivada');
+            
+            //Filtramos las imagenes correspondientes a la categoria
+            
+            if(categoria.dataset.etiqueta=="todos"){
+            	$('.cajaItem .img').show();
+            	$('.cajaItem .descripcion').show();
+            }
+
+            else{ 
+            	$('.cajaItem .img').hide();
+            	$('.cajaItem .descripcion').hide();
+
+            	$('.cajaItem img[data-categoria='+categoriaActual+']').show();
+            	$('.cajaItem .descripcion[data-categoria='+categoriaActual+']').show();
+            }
+            	});
+
+        });
+        
+        //Trabajamos sobre la imagen selecccionada
+
+       var imagenUnica = document.querySelector('.cajaPantallaUnica');
+
+        imagenes.forEach((imagen)=>{
+        	imagen.addEventListener('click',()=>{
+        		var ruta = imagen.getAttribute('src');
+
+        		document.querySelector('.cajaPantallaUnica .cajaImg .imgUnica').src=ruta;
+                
+                
+               imagenUnica.classList.add('activoPantallaUnica');
+
+            });
+   
+        });
+        
+        //Trabajamos sobre el boton cerrar
+        document.querySelector('#botonCerrar').addEventListener('click',()=>{
+                imagenUnica.classList.remove('activoPantallaUnica');
+	    });
+		   
+		
+	
+
+
+});
+
